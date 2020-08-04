1eeb8999520453e7b498e12dd3e410e4a2739799
Venkata <venkataegt@gmail.com>
2020-08-04 04:18:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# musician-app
+NodeJS / React sample app for AWS CI/CD pipeline tutorial
+
+https://www.youtube.com/watch?v=NwzJCSPSPZs
+"# users-app" 
---
+++ store/data.js
@@ -0,0 +1,38 @@
+// A collection of musicians where the key is the unique id
+module.exports = {
+  Decheke: {
+    firstName: 'Paul',
+    lastName: 'Decheke',
+    department: 'IT1',
+  },
+
+  Liang: {
+    firstName: 'Amy',
+    lastName: 'Liang',
+    department: 'IT2',
+  },
+
+  Smigelski: {
+    firstName: 'George',
+    lastName: 'Smigelski',
+    department: 'IT3',
+  },
+
+  Lewis: {
+    firstName: 'Scott',
+    lastName: 'Lewis',
+    department: 'IT4',
+  },
+
+  Volpe: {
+    firstName: 'Paul',
+    lastName: 'Volpe',
+    department: 'IT5',
+  },
+
+  Yella: {
+    firstName: 'Venkata',
+    lastName: 'Yella',
+    department: 'IT6',
+  },
+};
